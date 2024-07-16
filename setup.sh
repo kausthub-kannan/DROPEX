682d666570adc0acb9a708a9dee33874e7869b43
kausthub-kannan <kausthubkannan961@gmail.com>
2024-07-17 00:27:01 +0530

    fix: added setup.sh to download detr model

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 VENV_NAME=".venv"
+MODEL_PATH="simulation/server/models"
+DETR_FOLDER="$MODEL_PATH/detr"
+CONFIG_URL="https://huggingface.co/kausthubkannan17/dropex/raw/main/config.json"
+MODEL_URL="https://huggingface.co/kausthubkannan17/dropex/raw/main/model.safetensors"
 
 if ! command -v python3 &>/dev/null; then
     echo "Python 3 is not installed. Please install it and try again."
@@ -39,6 +43,26 @@ if [ -f "requirements.txt" ]; then
     echo "Requirements installed successfully."
 else
     echo "requirements.txt not found. No packages were installed."
+fi
+
+# Create detr folder and download the file
+echo "Creating 'detr' folder and downloading config.json..."
+mkdir -p $DETR_FOLDER
+wget -O "$DETR_FOLDER/config.json" $CONFIG_URL
+
+if [ $? -eq 0 ]; then
+    echo "File downloaded successfully to $DETR_FOLDER/config.json"
+else
+    echo "Failed to download the file. Please check the URL and try again."
+fi
+
+echo "Downloading model.safetensors..."
+wget -O "$DETR_FOLDER/model.safetensors" $MODEL_URL
+
+if [ $? -eq 0 ]; then
+    echo "File downloaded successfully to $DETR_FOLDER/model.safetensors"
+else
+    echo "Failed to download the file. Please check the URL and try again."
 fi
 
 echo "Setup complete."
